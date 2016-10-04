b149d0fbd8414547a9a94deac004e4d05f12566f
vasilrimar <vasil.rimar@hotmail.com>
2016-10-04 00:52:18 -0400

    Setup folder structure and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack-hot/only-dev-server',
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
